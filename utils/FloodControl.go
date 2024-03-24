0107a37319762886b60533c4bd40439e425be1ea
artyomkorchagin <steamartem@gmail.com>
2024-03-24 16:44:49 +0300

    config beta done

--- utils/FloodControl.go
+++ utils/FloodControl.go
@@ -1,1 +1,36 @@
 package utils
+
+import (
+	"gopkg.in/yaml.v2"
+	"os"
+	"time"
+)
+
+var (
+	configPath string = "./config.yaml"
+)
+
+type FloodControl struct {
+	MaxTries int `yaml:"maxTries"` // K - количество вызовов
+	Interval int `yaml:"interval"` // N - секунд
+	Counters map[int]int
+	Ticker   *time.Ticker
+}
+
+func NewFloodControl() (*FloodControl, error) {
+	fc := FloodControl{}
+	file, err := os.Open(configPath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	data := yaml.NewDecoder(file)
+
+	if err := data.Decode(&fc); err != nil {
+		return nil, err
+	}
+	fc.Ticker = time.NewTicker(time.Second)
+	fc.Counters = make(map[int]int)
+	return &fc, nil
+
+}
